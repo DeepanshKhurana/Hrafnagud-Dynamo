fbf498c3df7c9b5056520cc6ee219ad1f9f57d88
deepanshkhurana <deepanshkhurana@outlook.com>
2024-02-06 14:27:23 +0530

    chore: comment out local paths

--- plumber.R
+++ plumber.R
@@ -18,8 +18,8 @@ box::use(
 )
 
 box::use(
-  #  `Hrafnagud-Dynamo`/utils/dynamodb_utils[
-  utils/dynamodb_utils[
+  `Hrafnagud-Dynamo`/utils/dynamodb_utils[
+  # utils/dynamodb_utils[
     get_processed_table_data,
     get_table_schema,
     put_table_row,
