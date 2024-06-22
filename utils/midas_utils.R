8f9a6a7c9d5ee5ee18fa150f282c896b852390c5
deepanshkhurana <deepanshkhurana@outlook.com>
2024-06-22 23:13:51 +0530

    fix: get direct output for Midas APIs

--- utils/midas_utils.R
+++ utils/midas_utils.R
@@ -5,7 +5,12 @@ box::use(
     html_node,
     html_nodes
   ],
-  stringr[str_trim]
+  stringr[str_trim],
+  httr2[
+    request,
+    req_perform,
+    resp_body_json
+  ]
 )
 
 #' @export
@@ -14,25 +19,43 @@ box::use(
 #' @param type the type of data to get: buy, sell or both
 #' @return A named list with gold prices
 get_mmtc_price <- function(
-  web_link = "https://www.mmtcpamp.com/gold-silver-rate-today"
+  crawl = FALSE
 ) {
-  tryCatch({
-    html <- read_html(web_link)
-    sell_price <- as.numeric(html_text(html_node(html, "#sellGoldPrice")))
-    buy_price <- as.numeric(html_text(html_node(html, "#goldPrice")))
-    list(
-      "sell" = ifelse(is.na(sell_price), 0, sell_price),
-      "buy" = ifelse(is.na(buy_price), 0, buy_price)
-    )
-  },
-  error = function(e) {
-    message("Failed to fetch prices from the provided link. \
-            Setting prices to 0, 0.")
-    list(
-      sell = 0,
-      buy = 0
-    )
-  })
+  if (crawl) {
+    web_link <- "https://www.mmtcpamp.com/gold-silver-rate-today"
+    tryCatch({
+      html <- read_html(web_link)
+      sell_price <- as.numeric(html_text(html_node(html, "#sellGoldPrice")))
+      buy_price <- as.numeric(html_text(html_node(html, "#goldPrice")))
+    },
+    error = function(e) {
+      message(
+        "Failed to fetch prices from the provided link.
+        \ Setting prices to 0, 0."
+      )
+      list(
+        sell = 0,
+        buy = 0
+      )
+    })
+  } else {
+
+    web_link <- "https://cem.mmtcpamp.com/cms/getTodaysPagePrice"
+
+    response <- request(web_link) |>
+      req_perform() |>
+      resp_body_json()
+
+    buy_price <- response$data$attributes$TodaysPrice[[1]]
+    sell_price <- response$data$attributes$TodaysPrice[[2]]
+
+    buy_price <- buy_price$live_prices$data[[1]]$attributes$price
+    sell_price <- sell_price$live_prices$data[[1]]$attributes$price
+  }
+  list(
+    "sell" = ifelse(is.na(sell_price), 0, sell_price),
+    "buy" = ifelse(is.na(buy_price), 0, buy_price)
+  )
 }
 
 #' @export
