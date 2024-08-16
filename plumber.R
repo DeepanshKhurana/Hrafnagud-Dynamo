992cd8e268375f0b69ae0fd1df8aea2e2ef483ac
deepanshkhurana <deepanshkhurana@outlook.com>
2024-08-16 15:13:34 +0530

    chore: update for deployment

--- plumber.R
+++ plumber.R
@@ -19,24 +19,24 @@ box::use(
 )
 
 box::use(
-# `Hrafnagud-Dynamo`/utils/crud_utils[ # nolint
-  utils/crud_utils[ # nolint
+`Hrafnagud-Dynamo`/utils/crud_utils[ # nolint
+  # utils/crud_utils[ # nolint
     get_processed_table_data,
     get_table_schema,
     put_table_row,
     delete_table_row
   ],
-# `Hrafnagud-Dynamo`/utils/robin_utils[ # nolint
- utils/robin_utils[ # nolint
+`Hrafnagud-Dynamo`/utils/robin_utils[ # nolint
+ # utils/robin_utils[ # nolint
     load_sheet
   ],
-# `Hrafnagud-Dynamo`/utils/midas_utils[ # nolint
- utils/midas_utils[ # nolint
+`Hrafnagud-Dynamo`/utils/midas_utils[ # nolint
+ # utils/midas_utils[ # nolint
     get_mmtc_price,
     get_bullions_price
   ],
-# `Hrafnagud-Dynamo`/utils/ebenezer_utils[ # nolint
- utils/ebenezer_utils[ # nolint
+`Hrafnagud-Dynamo`/utils/ebenezer_utils[ # nolint
+ # utils/ebenezer_utils[ # nolint
     calculate_portfolio,
     summarise_portfolio,
     calculate_funds,
@@ -46,12 +46,12 @@ box::use(
     summarise_mmtc,
     summarise_sgbs
   ],
-# `Hrafnagud-Dynamo`/utils/chronos_utils[ # nolint
-utils/chronos_utils[ # nolint
+`Hrafnagud-Dynamo`/utils/chronos_utils[ # nolint
+# utils/chronos_utils[ # nolint
     get_combined_calendars
   ],
-# `Hrafnagud-Dynamo`/utils/fogg_utils[ # nolint
-utils/fogg_utils[ # nolint
+`Hrafnagud-Dynamo`/utils/fogg_utils[ # nolint
+# utils/fogg_utils[ # nolint
     get_labelled_tasks_df,
     get_tasks_analysis
   ],
